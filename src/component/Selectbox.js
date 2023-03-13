50381a9f643f8f8c876d89c836658e399aab8363
krakhmadi <14krakhmadi@gmail.com>
2023-03-13 17:06:06 +0700

    full version latihan react

--- src/component/Selectbox.js
+++ src/component/Selectbox.js
@@ -14,18 +14,4 @@ const SelectBox = ({ label = "", title = "", data = [], ...props }) => {
     )
 }
 
-// const SelectBox = ({ label = "", title = "", data = [], ...props }) => {
-//     return (
-//         <div className="form-group col-md-3 d-flex" style={{ gap:"8px", flexDirection:'column', marginTop:"8px", marginBottom:"8px" }}>
-//             <label className='paragraph-form'>{label}</label>
-//             <select {...props} className="form-control" id="inputGroupSelect01">
-//                 <option selected value={""} disabled>{title}</option>
-//                 {data.map((item, index) => (
-//                     <option key={index} value={item.value}>{item.label}</option>
-//                 ))}
-//             </select>
-//         </div>
-//     )
-// }
-
 export default SelectBox;
